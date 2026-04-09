f91617c8bf034cadbfe8d5cd81417c4f00fcec3d
kubow <kube.kubow@gmail.com>
2026-04-09 14:35:33 +0200

    feat: purge flat react content

--- src/lib/i18n/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as i18n, LANGUAGE_STORAGE_KEY, SUPPORTED_LANGUAGES } from './i18n';
-export type { AppLanguage } from './i18n';
--- src/lib/tauri/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './types';
-export * from './chartPayload';
-export * from './workspace';
